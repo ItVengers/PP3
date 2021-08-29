266068aa4908b135674c2990f7f81d68f6378be9
PabloAntepazo <pabloantepazo@gmail.com>
2021-08-29 20:00:52 -0300

    Pagina subida a la nubeeee

--- PP3 Final VERSION/cliente/src/environments/environment.prod.ts
+++ PP3 Final VERSION/cliente/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  API_URI: "http://localhost:3000"
+  //API_URI: "http://localhost:3000"
+  API_URI: "https://backendcadenahotelera.herokuapp.com"
+
 };

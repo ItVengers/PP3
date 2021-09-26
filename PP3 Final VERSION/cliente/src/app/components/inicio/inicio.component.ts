dbc7a4e366809b53b77862aad857c0c20f56d9bb
PabloAntepazo <pabloantepazo@gmail.com>
2021-09-26 19:40:38 -0300

    Sacando formato de fecha

--- PP3 Final VERSION/cliente/src/app/components/inicio/inicio.component.ts
+++ PP3 Final VERSION/cliente/src/app/components/inicio/inicio.component.ts
@@ -2,22 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { UsuariosService } from 'src/app/services/usuarios.service';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
+import { Pipe, PipeTransform } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css'],
-  providers: [MessageService],
+  providers: [MessageService, DatePipe],
 })
 export class InicioComponent implements OnInit {
 
-  constructor(private usuariosService: UsuariosService, private router: Router, private messageService: MessageService) { }
+  constructor(private usuariosService: UsuariosService, private router: Router, private messageService: MessageService, private datePipe: DatePipe) {
+    this.minDate = new Date();
+    this.maxDate = new Date();
+    this.minDate.setDate(this.minDate.getDate());
+    this.maxDate.setDate(this.maxDate.getDate() + 365);
+
+  }
 
   hoteles: any = [];
   zona = { zona_id: "" };
 
   seleccionado: string = "";
   fecha: string = "";
+  minDate: Date;
+  maxDate: Date;
+
 
   ngOnInit(): void {
 
@@ -80,5 +91,18 @@ export class InicioComponent implements OnInit {
       }
     )
     // this.traerHoteles();
+  }
+
+
+  //PRUEBAS PARA SACAR FORMATO DEL DATAPICKER
+
+  mostrarFormatoFecha() {
+
+    let date = new Date(this.fecha).toLocaleDateString();
+
+    let dates = this.datePipe.transform(this.fecha, 'YYYY/MM/dd');
+    console.log(date);
+    console.log(dates);
+    console.log(this.fecha);
   }
 }

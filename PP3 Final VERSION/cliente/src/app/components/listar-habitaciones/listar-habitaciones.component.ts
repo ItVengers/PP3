2c0ac773f89ec63790508c352649d70ce26075a3
javier1012 <javierang83@gmail.com>
2021-09-26 02:04:52 -0300

    pruebas

--- PP3 Final VERSION/cliente/src/app/components/listar-habitaciones/listar-habitaciones.component.ts
+++ PP3 Final VERSION/cliente/src/app/components/listar-habitaciones/listar-habitaciones.component.ts
@@ -1,27 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuariosService } from '../../services/usuarios.service';
+// import {DataViewModule} from 'primeng/dataview';
 
 @Component({
-  selector: 'app-listar-habitaciones',
-  templateUrl: './listar-habitaciones.component.html',
-  styleUrls: ['./listar-habitaciones.component.css']
-})
-export class ListarHabitacionesComponent implements OnInit {
-  habitaciones:any = [];
-  revelar:boolean = true;
+    selector: 'app-listar-habitaciones',
+    templateUrl: './listar-habitaciones.component.html',
+    styleUrls: ['./listar-habitaciones.component.css']
+  })
+  export class ListarHabitacionesComponent implements OnInit {
+    habitaciones:any = [];
+    revelar:boolean = true;
+  
+    constructor(private usuariosService:UsuariosService){	}
+  
+    ngOnInit(){
+          this.usuariosService.listarHabitaciones().subscribe(
+              res => { 
+          this.habitaciones = res; 
+          console.log(res)
+        },      
+              err => console.log(err)
+          )
+      }
 
-  constructor(private usuariosService:UsuariosService){	}
+  }
+// import {Injectable} from 'angular2/core';
+// import {Http, Response} from 'angular2/http';
+// import {Car} from '../domain/car';
 
-  ngOnInit(){
-		this.usuariosService.listarHabitaciones().subscribe(
-			res => { 
-        this.habitaciones = res; 
-        console.log(res)
-      },      
-			err => console.log(err)
-		)
-	}
+// @Injectable()
+// export class CarService {
 
-}
+//     constructor(private http: Http) {}
+
+//     getCarsLarge() {
+//         return this.http.get('/showcase/resources/data/cars-large.json')
+//                     .toPromise()
+//                     .then(res => <Car[]> res.json().data)
+//                     .then(data => { return data; });
+//     }
+// }
+
+
 
 

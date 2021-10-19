bb037e13e6a211bae717463997bc6a812c0e744a
PabloAntepazo <pabloantepazo@gmail.com>
2021-10-19 19:31:38 -0300

    Pequeña actualizacion

--- PP3 Final VERSION/cliente/src/app/components/usuarios-reservar/usuarios-reservar.component.ts
+++ PP3 Final VERSION/cliente/src/app/components/usuarios-reservar/usuarios-reservar.component.ts
@@ -44,4 +44,12 @@ export class UsuariosReservarComponent implements OnInit {
   }
 
   datosReserva() {}
+
+  enviarDatosReserva(datos: any){
+
+    this.router.navigate(['../usuarios/listarhabitaciones']);
+
+
+
+  }
 }

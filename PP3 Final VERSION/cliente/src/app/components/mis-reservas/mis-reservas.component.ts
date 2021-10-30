d7d20707c2661db041583d11c495bbb3176fb0b6
PabloAntepazo <pabloantepazo@gmail.com>
2021-10-30 17:24:35 -0300

    pequeños cambios

--- PP3 Final VERSION/cliente/src/app/components/mis-reservas/mis-reservas.component.ts
+++ PP3 Final VERSION/cliente/src/app/components/mis-reservas/mis-reservas.component.ts
@@ -3,6 +3,8 @@ import { UsuariosService } from 'src/app/services/usuarios.service';
 import { Router } from '@angular/router';
 import { state } from '@angular/animations';
 import { Location } from '@angular/common';
+import { MessageService } from 'primeng/api';
+
 
 @Component({
   selector: 'app-mis-reservas',
@@ -11,11 +13,7 @@ import { Location } from '@angular/common';
 })
 
 export class MisReservasComponent implements OnInit {
-  reserva: any = [];
-  // dayjs(fecha)
-  // revelar:boolean = true;
-
-  //datosreserva: any = "";
+  idPersona = localStorage.getItem('idPersona');
 
   datosreserva = {
     idReserva: "", fechaDesde: "", fechaHasta: "", fechaReserva: "", habitacion_id: "",
@@ -23,39 +21,61 @@ export class MisReservasComponent implements OnInit {
   };
 
 
-  constructor(private usuariosService: UsuariosService, private router: Router, private location: Location) {
-    const navigation = this.router.getCurrentNavigation();
-    const state = navigation?.extras.state as {
-      idReserva: string,
-      fechaDesde: string,
-      fechaHasta: string,
-      fechaReserva: string,
-      habitacion_id: string,
-      cantidadPax: string,
-      persona_id: string,
-      precioTotal: string
-    };
+  constructor(private usuariosService: UsuariosService,
+    private messageService: MessageService,
+    private router: Router, private location: Location) {
+    // const navigation = this.router.getCurrentNavigation();
+    // const state = navigation?.extras.state as {
+    //   idReserva: string,
+    //   fechaDesde: string,
+    //   fechaHasta: string,
+    //   fechaReserva: string,
+    //   habitacion_id: string,
+    //   cantidadPax: string,
+    //   persona_id: string,
+    //   precioTotal: string
+    // };
 
-    this.datosreserva.cantidadPax = state.cantidadPax;
-    this.datosreserva.precioTotal = state.precioTotal;
-    this.datosreserva.persona_id = state.persona_id;
-    this.datosreserva.habitacion_id = state.habitacion_id;
-    this.datosreserva.fechaReserva = state.fechaReserva;
-    this.datosreserva.fechaHasta = state.fechaHasta;
-    this.datosreserva.fechaDesde = state.fechaDesde;
-    this.datosreserva.idReserva = state.idReserva;
+    // this.datosreserva.cantidadPax = state.cantidadPax;
+    // this.datosreserva.precioTotal = state.precioTotal;
+    // this.datosreserva.persona_id = state.persona_id;
+    // this.datosreserva.habitacion_id = state.habitacion_id;
+    // this.datosreserva.fechaReserva = state.fechaReserva;
+    // this.datosreserva.fechaHasta = state.fechaHasta;
+    // this.datosreserva.fechaDesde = state.fechaDesde;
+    // this.datosreserva.idReserva = state.idReserva;
   }
 
   ngOnInit(): void {
     console.log()
-    this.mostrarDatos();
+    this.mostrarDatos(this.idPersona);
   }
 
-  mostrarDatos() {
-    console.log(this.datosreserva);
+  mostrarDatos(id: any) {
+    this.usuariosService.peticionDatosUsuarios(id).subscribe(
+      (res) => {
+        let result: any = res;
+        console.log(res);
+        this.datosreserva = result;
+
+        // this.messageService.add({
+        //   severity: 'success',
+        //   summary: 'Bienvenido!!!',
+        //   detail: result,
+        // });
+      },
+      (err) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: err.statusText,
+          detail: err.error.message,
+        });
+        console.log(err.error.message);
+      }
+    )
   }
 
-  goBack(){
+  goBack() {
     this.location.back();
   }
 }
